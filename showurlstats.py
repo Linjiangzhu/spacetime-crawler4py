4e58e3807c33b914b12d6239b33b3ffdb8fcfb21
Linjiangzhu <yuyuex97@gmail.com>
2020-02-12 19:34:06 -0800

    start handle stats

--- showurlstats.py
+++ showurlstats.py
@@ -1,5 +1,24 @@
 from os import path
 
-if __name__ == "__main__":
-    if path.exists("word")
+def getMaxWordCount(filepath):
+    maxCount = 0
+    maxCountURL = ""
+    with open(filepath) as f:
+        for line in f:
+            el = line.split()
+            num = int(el[0])
+            url = el[1]
+            if num > maxCount:
+                maxCount = num
+                maxCountURL = url
+    print(f"Web Page with most number of words:\n{maxCountURL} {maxCount} words")
 
+
+
+if __name__ == "__main__":
+    if path.exists("word-count.txt"):
+        getMaxWordCount("word-count.txt")
+    if path.exists("word-token.txt"):
+        pass
+    if path.exists("url-group.txt"):
+        pass
